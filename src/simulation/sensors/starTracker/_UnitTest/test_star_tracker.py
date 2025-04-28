91b414fdeaf28d8d503dad50d5e88142f0e15dd5
Leah Kiner <leahannkiner@gmail.com>
2025-04-28 11:44:45 -0600

    Add platform angular velocity check to unit test

--- src/simulation/sensors/starTracker/_UnitTest/test_star_tracker.py
+++ src/simulation/sensors/starTracker/_UnitTest/test_star_tracker.py
@@ -40,7 +40,8 @@ def setRandomWalk(self, senNoiseStd = 0.0, errorBounds = [[1e6],[1e6],[1e6]]):
 @pytest.mark.parametrize("useFlag, testCase", [
     (False, 'basic'),
     (False, 'noise'),
-    (False, 'walk bounds')
+    (False, 'walk bounds'),
+    (False, 'angular velocity check')
 ])
 def test_starTracker(show_plots, useFlag, testCase):
     unitTaskName = "unitTask"
@@ -101,6 +102,12 @@ def test_starTracker(show_plots, useFlag, testCase):
         trueVector['qInrtl2Case'] = [walkBound + noiseStd*3] * 3
         trueVector['timeTag'] = np.arange(0, 0+simStopTime*1E9, unitProcRate_s*1E9)
 
+    # This test checks the computed platform rate
+    elif testCase == 'angular velocity check':
+        simStopTime = unitProcRate_s
+        sigma = np.array([0, 0, 0])
+        scStatesMessageData.sigma_BN = sigma
+
     else:
         raise Exception('invalid test case')
 
@@ -116,14 +123,50 @@ def test_starTracker(show_plots, useFlag, testCase):
     unitSim.ConfigureStopTime(macros.sec2nano(simStopTime))
     unitSim.ExecuteSimulation()
 
+    # Run additional simulation chunks for angular velocity test check
+    sigma_BNList = [np.array([0.0, 0.0, 0.0])]
+    if testCase == 'angular velocity check':
+        rotAxis_N = np.array([1.0, 0.0, 0.0])  # Hub rotation axis
+        prvAngleList = np.array([0.0, 0.25, 0.6, 1.0, 1.1])  # Truth hub attitudes
+
+        for idx in range(1, len(prvAngleList)):
+            # Compute hub inertial attitude
+            prv_BN = prvAngleList[idx] * rotAxis_N
+            sigma_BN = np.array(rbk.PRV2MRP(prv_BN))
+            sigma_BNList.append(sigma_BN)
+
+            # Update and connect the sc state message to the star tracker module
+            scStatesMessageData = messaging.SCStatesMsgPayload()
+            scStatesMessageData.r_BN_N = [0, 0, 0]
+            scStatesMessageData.v_BN_N = [0, 0, 0]
+            scStatesMessageData.sigma_BN = sigma_BN
+            scStatesMessageData.omega_BN_B = [0, 0, 0]
+            scStatesMessageData.TotalAccumDVBdy = [0, 0, 0]
+            scStatesMessageData.MRPSwitchCount = 0
+            scStatesMessage = messaging.SCStatesMsg().write(scStatesMessageData)
+            StarTracker.scStateInMsg.subscribeTo(scStatesMessage)
+
+            # Execute simulation chunk for updated spacecraft attitude
+            simStopTime = simStopTime + unitProcRate_s
+            unitSim.ConfigureStopTime(macros.sec2nano(simStopTime))
+            unitSim.ExecuteSimulation()
+
+        sigma_BNList = np.vstack(sigma_BNList)
+
     # Extract logged data for test check
+    timespan = macros.NANO2SEC * starTrackerSensorMsgDataLog.times()  # [s]
     beta_CN = starTrackerSensorMsgDataLog.qInrtl2Case
+    omega_CN_C = macros.R2D * starTrackerSensorMsgDataLog.omega_CN_C  # [rad/s]
 
     # Convert quaternion output to prv
     prv_CN = np.zeros([int(simStopTime/unitProcRate_s)+1, 3])
     for i in range(0, int(simStopTime/unitProcRate_s)+1):
-        prv_CN[i] = rbk.EP2PRV(beta_CN[i])
+        if not np.allclose(beta_CN[i], [1.0, 0.0, 0.0, 0.0]):
+            prv_CN[i] = rbk.EP2PRV(beta_CN[i])
+        else:
+            prv_CN[i] = [0.0, 0.0, 0.0]
 
+    accuracy = 1e-6
     if testCase == 'noise':
         boundArray = np.full((int(simStopTime/unitProcRate_s)+1), 0.01)
         for i in range(0, 3):
@@ -134,13 +177,49 @@ def test_starTracker(show_plots, useFlag, testCase):
             np.testing.assert_array_less(np.abs(np.std(prv_CN[:, i]) - trueVector['qInrtl2Case'][i]),
                                          boundArray,
                                          verbose=True)
+
     elif testCase == 'walk bounds':
         for i in range(0, 3):
             np.testing.assert_array_less(np.max(np.abs(np.asarray(prv_CN[i]))),
                                          trueVector['qInrtl2Case'][i],
                                          verbose=True)
+
+    elif testCase == 'angular velocity check':
+        # Check computed platform angular velocity
+        omega_CN_CTruth = [np.zeros((1, 3))]
+        previousSimTime = timespan[0]
+        currentSimTime = timespan[0]
+        for idx in range(1, len(timespan)):
+            # Group quaternion components without beta_0
+            epsilon_CN = np.array(beta_CN[idx, 1:]).reshape(3, 1)
+            epsilonPrevious_CN = np.array(beta_CN[idx-1, 1:]).reshape(3, 1)
+
+            # Determine CRPs (Gibbs Vector)
+            q_CN = epsilon_CN / beta_CN[idx, 0]
+            qPrevious_CN = epsilonPrevious_CN / beta_CN[idx-1, 0]
+
+            # Compute qDot_PN
+            previousSimTime = currentSimTime
+            currentSimTime = timespan[idx]
+            dt = currentSimTime - previousSimTime
+            qDot_CN = (q_CN - qPrevious_CN) / dt
+
+            # Solve for platform rate using Eq. 3.137 from Schaub and Junkins Pg 120
+            qTilde_CN = rbk.v3Tilde(q_CN.flatten())
+            qTilde_CN = np.array(qTilde_CN)
+            I = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+            omega_cn_c = (2.0 / (1.0 + np.dot(q_CN.flatten(), q_CN.flatten()))) * (I - qTilde_CN) @ qDot_CN
+            omega_cn_c = omega_cn_c.reshape(1, 3)
+            omega_CN_CTruth.append(macros.R2D * omega_cn_c)  # [deg]
+
+        omega_CN_CTruth = np.vstack(omega_CN_CTruth)
+
+        np.testing.assert_allclose(omega_CN_C,
+                                   omega_CN_CTruth,
+                                   atol=accuracy,
+                                   verbose=True)
+
     else:
-        accuracy = 1e-6
         for i in range(0, len(trueVector['qInrtl2Case'])):
             np.testing.assert_allclose(beta_CN[i],
                                        trueVector['qInrtl2Case'][i],
