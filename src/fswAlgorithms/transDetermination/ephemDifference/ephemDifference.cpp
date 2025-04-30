99883104311b53fd0c291687e9d30861d966cbbf
Chun-Wei Kong <j2855001@gmail.com>
2025-04-30 07:33:10 -0600

    Refactor to C++ compliant

--- src/fswAlgorithms/transDetermination/ephemDifference/ephemDifference.cpp
+++ src/fswAlgorithms/transDetermination/ephemDifference/ephemDifference.cpp
@@ -18,14 +18,12 @@
  */
 
 #include "fswAlgorithms/transDetermination/ephemDifference/ephemDifference.h"
-#include "architecture/utilities/linearAlgebra.h"
 
 /*! @brief This method resets the module.
  @return void
  @param callTime The clock time at which the function was called (nanoseconds)
  */
-void EphemDifference::reset(uint64_t callTime)
-{
+void EphemDifference::reset(uint64_t callTime) {
     // check if the required message has not been connected
     if (!this->ephBaseInMsg.isLinked()) {
         this->bskLogger.bskLog(BSK_ERROR, "Error: ephemDifference.ephBaseInMsg wasn't connected.");
@@ -43,8 +41,8 @@ void EphemDifference::reset(uint64_t callTime)
 
     if (this->ephBdyCount == 0) {
         this->bskLogger.bskLog(BSK_WARNING,
-                                "Your outgoing ephemeris message count is zero. "
-                                "Be sure to specify desired output messages.");
+                               "Your outgoing ephemeris message count is zero. "
+                               "Be sure to specify desired output messages.");
     }
 }
 
@@ -54,21 +52,17 @@ void EphemDifference::reset(uint64_t callTime)
  @return void
  @param callTime The clock time at which the function was called (nanoseconds)
  */
-void EphemDifference::updateState(uint64_t callTime)
-{
+void EphemDifference::updateState(uint64_t callTime) {
     // read input msg
     EphemerisMsgPayload tmpBaseEphem = this->ephBaseInMsg();
 
-    for(uint32_t i = 0; i < this->ephBdyCount; i++)
-    {
+    for (uint32_t i = 0; i < this->ephBdyCount; i++) {
         auto tmpEphStore = this->changeBodies[i].ephInMsg();
 
-        v3Subtract(tmpEphStore.r_BdyZero_N,
-                   tmpBaseEphem.r_BdyZero_N,
-                   tmpEphStore.r_BdyZero_N);
-        v3Subtract(tmpEphStore.v_BdyZero_N,
-                   tmpBaseEphem.v_BdyZero_N,
-                   tmpEphStore.v_BdyZero_N);
+        for (uint32_t j = 0; j < 3; j++) {
+            tmpEphStore.r_BdyZero_N[j] -= tmpBaseEphem.r_BdyZero_N[j];
+            tmpEphStore.v_BdyZero_N[j] -= tmpBaseEphem.v_BdyZero_N[j];
+        }
 
         this->changeBodies[i].ephOutMsg.write(&tmpEphStore, moduleID, callTime);
     }
--- src/fswAlgorithms/transDetermination/ephemDifference/ephemDifference.h
+++ src/fswAlgorithms/transDetermination/ephemDifference/ephemDifference.h
@@ -20,33 +20,33 @@
 #ifndef _EPHEM_DIFFERENCE_H_
 #define _EPHEM_DIFFERENCE_H_
 
-#define MAX_NUM_CHANGE_BODIES 10
-
 #include "architecture/_GeneralModuleFiles/sys_model.h"
 #include "architecture/messaging/messaging.h"
 #include "architecture/msgPayloadDefC/EphemerisMsgPayload.h"
-
 #include "architecture/utilities/bskLogging.h"
+#include <array>
+#include <cstdint>
 
+inline constexpr int MAX_NUM_CHANGE_BODIES = 10;
 
 /*! @brief Container with paired input/output message names and IDs */
-typedef struct{
+typedef struct {
     ReadFunctor<EphemerisMsgPayload> ephInMsg;  //!< [-] Input name for the ephemeris message
-    Message<EphemerisMsgPayload> ephOutMsg; //!< [-] The name converted output message
-}EphemChangeConfig;
+    Message<EphemerisMsgPayload> ephOutMsg;     //!< [-] The name converted output message
+} EphemChangeConfig;
 
 /*! @brief Container holding ephemDifference module variables */
 class EphemDifference : public SysModel {
-public:
+   public:
     void updateState(uint64_t callTime) override;
     void reset(uint64_t callTime) override;
 
-    ReadFunctor<EphemerisMsgPayload> ephBaseInMsg; //!< base ephemeris input message name
-    EphemChangeConfig changeBodies[MAX_NUM_CHANGE_BODIES]; //!< [-] The list of bodies to change out
+    ReadFunctor<EphemerisMsgPayload> ephBaseInMsg;                        //!< base ephemeris input message name
+    std::array<EphemChangeConfig, MAX_NUM_CHANGE_BODIES> changeBodies{};  //!< [-] The list of bodies to change out
 
-    uint32_t ephBdyCount; //!< [-] The number of ephemeris bodies we are changing
+    uint32_t ephBdyCount;  //!< [-] The number of ephemeris bodies we are changing
 
-    BSKLogger bskLogger{}; //!< BSK Logging
+    BSKLogger bskLogger{};  //!< BSK Logging
 };
 
 #endif
