b856293515280ddcd54b254d759490bce51ebca6
Leah Kiner <leahannkiner@gmail.com>
2025-05-20 16:02:46 -0600

    Formatting and comment adjustments to unit test

--- src/fswAlgorithms/attControl/rateDamp/_UnitTest/test_rateDamp.py
+++ src/fswAlgorithms/attControl/rateDamp/_UnitTest/test_rateDamp.py
@@ -25,12 +25,9 @@ from Basilisk.utilities import macros
 from Basilisk.architecture import messaging
 
 @pytest.mark.parametrize("P", [0.0, 2.0])
-@pytest.mark.parametrize("accuracy", [1e-12])
-def test_rateDamp(show_plots, P, accuracy):
-
+def test_rateDamp(show_plots, P):
     unitTaskName = "unitTask"
     unitProcessName = "TestProcess"
-    bskLogging.setDefaultLogLevel(bskLogging.BSK_WARNING)
 
     # Create a sim module as an empty container
     unitTestSim = SimulationBaseClass.SimBaseClass()
@@ -40,47 +37,37 @@ def test_rateDamp(show_plots, P, accuracy):
     testProc = unitTestSim.CreateNewProcess(unitProcessName)
     testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
 
-    # Construct algorithm and associated C++ container
+    # Create the rateDamp module
     attControl = rateDamp.RateDamp()
     attControl.setRateGain(P)
     attControl.modelTag = "rateDamp"
-
-    # Add test module to runtime call list
     unitTestSim.AddModelToTask(unitTaskName, attControl)
 
-    # Initialize the test module configuration data
-    # These will eventually become input messages
-
+    # Create the rateDamp module input navigation message
     omega_BN_B = np.array([0.1, -0.2, 0.3])
-
-    # Create input navigation message
     NavAttMessageData = messaging.NavAttMsgPayload()
     NavAttMessageData.omega_BN_B = omega_BN_B
     NavAttMsg = messaging.NavAttMsg().write(NavAttMessageData)
     attControl.attNavInMsg.subscribeTo(NavAttMsg)
 
-    # Setup logging on the test module output message so that we get all the writes to it
+    # Set up data logging
     dataLog = attControl.cmdTorqueOutMsg.recorder()
     unitTestSim.AddModelToTask(unitTaskName, dataLog)
 
-    # Need to call the self-init and cross-init methods
+    # Run the simulation
     unitTestSim.InitializeSimulation()
-
-    # Set the simulation time.
     unitTestSim.ConfigureStopTime(macros.sec2nano(1.0))
-
-    # Begin the simulation time run set above
     unitTestSim.ExecuteSimulation()
 
+    # Extract the logged data
     moduleOutput = dataLog.torqueRequestBody[0]
 
+    # Compute the truth command torque
     truth = -attControl.getRateGain() * omega_BN_B
 
-    # set the filtered output truth states
+    # Check that the module-output command torque vector matches the computed truth value
+    accuracy = 1e-12
     np.testing.assert_allclose(moduleOutput, truth, rtol=0, atol=accuracy, verbose=True)
 
-    return
-
-
 if __name__ == "__main__":
-    test_rateDamp(False, 1, 1e-12)
+    test_rateDamp(False, 1)
