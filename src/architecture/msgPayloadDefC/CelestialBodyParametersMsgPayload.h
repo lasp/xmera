4e559e768861fffa9049a02d0514d85cb1a22f57
tteil <thibaud.teil@lasp.colorado.edu>
2025-07-21 22:02:13 -0600

    add more detailed perlin noise variables

--- src/architecture/msgPayloadDefC/CelestialBodyParametersMsgPayload.h
+++ src/architecture/msgPayloadDefC/CelestialBodyParametersMsgPayload.h
@@ -29,7 +29,10 @@ typedef struct {
     char bodyName[MAX_STRING_LENGTH];
     char shapeModel[MAX_STRING_LENGTH];
     double sigma_BN[3];
-    double perlinNoise;
+    int perlinNoiseOctaveCount;
+    double perlinNoiseBaseFrequency;
+    double perlinNoiseBaseAmplitude;
+    double perlinNoisePersistence;
     double proceduralRocks;
     char brdf[MAX_STRING_LENGTH];
     double reflectanceParameters[MAX_PARAMETER_LENGTH];
