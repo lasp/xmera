6891d1f5d55a5dee683de7d6cdbe13161c5ee76c
Chun-Wei Kong <j2855001@gmail.com>
2025-07-31 10:44:29 -0600

    Factor out core algorithm

---
+++ src/fswAlgorithms/attGuidance/flybyPoint/flybyPointAlgorithm.cpp
@@ -0,0 +1,219 @@
+/*
+ ISC License
+
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#include "fswAlgorithms/attGuidance/flybyPoint/flybyPointAlgorithm.h"
+#include "architecture/utilities/avsEigenSupport.h"
+#include "architecture/utilities/macroDefinitions.h"
+#include "architecture/utilities/rigidBodyKinematics.hpp"
+
+/*! This method is used to reset the module.
+ @return void
+ */
+void FlybyPointAlgorithm::reset() {
+    this->lastFilterReadTime = 0;
+    this->firstRead = true;
+}
+
+/*! This function computes a reference attitude frame for a spacecraft in relative motion about a small body.
+ @return AttRefMsgPayload
+ @param currentSimNanos The current simulation time for system
+ @param r_BN_N The relative position state
+ @param v_BN_N The relative velocity state
+ */
+AttRefMsgPayload FlybyPointAlgorithm::updateState(uint64_t currentSimNanos,
+                                                  const Eigen::Vector3d &r_BN_N,
+                                                  const Eigen::Vector3d &v_BN_N) {
+    /*! compute dt from current time and last filter read time and get new states*/
+    this->dt = (currentSimNanos - this->lastFilterReadTime) * NANO2SEC;
+    if ((this->dt >= this->timeBetweenFilterData) || this->firstRead) {
+        /*! If this is the first read, seed the algorithm with the solution  */
+        if (this->firstRead) {
+            this->timeOfFirstRead = currentSimNanos * NANO2SEC;
+            this->firstNavPosition = r_BN_N;
+            this->firstNavVelocity = v_BN_N;
+            this->computeFlybyParameters(r_BN_N, v_BN_N);
+            this->computeRN(r_BN_N, v_BN_N);
+            this->firstRead = false;
+        }
+        /*! Protect against bad new solutions by checking validity */
+        else if (this->checkValidity(currentSimNanos, r_BN_N, v_BN_N)) {
+            /*! update flyby parameters and guidance frame */
+            this->computeFlybyParameters(r_BN_N, v_BN_N);
+            this->computeRN(r_BN_N, v_BN_N);
+
+            /*! update lastFilterReadTime to current time and dt to zero */
+            this->lastFilterReadTime = currentSimNanos;
+            this->dt = 0;
+        }
+    }
+    auto [sigma_RN, omega_RN_N, omegaDot_RN_N] = this->computeGuidanceSolution();
+    AttRefMsgPayload attMsgBuffer{};
+
+    eigenVector3d2CArray(sigma_RN, attMsgBuffer.sigma_RN);
+    eigenVector3d2CArray(omega_RN_N, attMsgBuffer.omega_RN_N);
+    eigenVector3d2CArray(omegaDot_RN_N, attMsgBuffer.domega_RN_N);
+    return attMsgBuffer;
+}
+
+void FlybyPointAlgorithm::computeFlybyParameters(const Eigen::Vector3d &r_BN_N, const Eigen::Vector3d &v_BN_N) {
+    this->f0 = v_BN_N.norm() / r_BN_N.norm();
+
+    /*! compute radial (ur_N), velocity (uv_N), along-track (ut_N), and out-of-plane (uh_N) unit direction vectors */
+    Eigen::Vector3d ur_N = r_BN_N.normalized();
+    Eigen::Vector3d uv_N = v_BN_N.normalized();
+
+    Eigen::Vector3d uh_N = ur_N.cross(uv_N).normalized();
+    Eigen::Vector3d ut_N = uh_N.cross(ur_N).normalized();
+
+    // compute flight path angle at the time of read
+    this->gamma0 = std::atan(v_BN_N.dot(ur_N) / v_BN_N.dot(ut_N));
+}
+
+bool FlybyPointAlgorithm::checkValidity(uint64_t currentSimNanos,
+                                        const Eigen::Vector3d &r_BN_N,
+                                        const Eigen::Vector3d &v_BN_N) const {
+    bool valid = true;
+    Eigen::Vector3d ur_N = r_BN_N.normalized();
+    Eigen::Vector3d uv_N = v_BN_N.normalized();
+    /*! assert r and v are not collinear (collision trajectory) */
+    if (std::abs(1 - ur_N.dot(uv_N)) < this->toleranceForCollinearity) {
+        valid = false;
+    }
+
+    /*! check if the predicted rate exceeds the maximum rate of the spacecraft */
+    double distanceClosestApproach = -r_BN_N.norm() * std::sin(this->gamma0);
+    double maxPredictedRate = v_BN_N.norm() / distanceClosestApproach * 180 / M_PI;
+    if (maxPredictedRate > this->maxRate && this->maxRate > 0) {
+        valid = false;
+    }
+    /*! check if the predicted acceleration exceeds the maximum acceleration of the spacecraft */
+    double maxPredictedAcceleration =
+        3 * std::sqrt(3) / 8 * pow(v_BN_N.norm() / distanceClosestApproach, 2) * 180 / M_PI;
+    if (maxPredictedAcceleration > this->maxAcceleration && this->maxAcceleration > 0) {
+        valid = false;
+    }
+    /*! check if the position error exceeds a-priori sigma bound */
+    double deltaT = currentSimNanos * NANO2SEC - this->timeOfFirstRead;
+    double deltaPositionNorm = (r_BN_N - (this->firstNavPosition + deltaT * this->firstNavVelocity)).norm();
+    if (deltaPositionNorm > this->positionKnowledgeSigma && this->positionKnowledgeSigma > 0) {
+        valid = false;
+    }
+
+    return valid;
+}
+
+void FlybyPointAlgorithm::computeRN(const Eigen::Vector3d &r_BN_N, const Eigen::Vector3d &v_BN_N) {
+    /*! compute radial (ur_N), velocity (uv_N), along-track (ut_N), and out-of-plane (uh_N) unit direction vectors */
+    Eigen::Vector3d ur_N = r_BN_N.normalized();
+    Eigen::Vector3d uv_N = v_BN_N.normalized();
+
+    Eigen::Vector3d uh_N = ur_N.cross(uv_N).normalized();
+    Eigen::Vector3d ut_N = uh_N.cross(ur_N).normalized();
+
+    /*! compute inertial-to-reference DCM at time of read */
+    this->R0N.row(0) = ur_N;
+    this->R0N.row(1) = ut_N;
+    this->R0N.row(2) = uh_N;
+}
+
+std::tuple<Eigen::Vector3d, Eigen::Vector3d, Eigen::Vector3d> FlybyPointAlgorithm::computeGuidanceSolution() const {
+    /*! compute DCM (RtR0) of reference frame from last read time */
+    double theta = std::atan(std::tan(this->gamma0) + this->f0 / std::cos(this->gamma0) * this->dt) - this->gamma0;
+    Eigen::Vector3d PRV_theta;
+    PRV_theta << 0, 0, theta;
+    Eigen::Matrix3d RtR0 = prvToDcm(PRV_theta);
+
+    /*! compute DCM of reference frame at time t_0 + dt with respect to inertial frame */
+    Eigen::Matrix3d RtN = RtR0 * this->R0N;
+
+    /*! compute scalar angular rate and acceleration of the reference frame in R-frame coordinates */
+    double den = (this->f0 * this->f0 * this->dt * this->dt + 2 * this->f0 * sin(this->gamma0) * this->dt + 1);
+    double thetaDot = this->f0 * cos(this->gamma0) / den;
+    double thetaDDot =
+        -2 * this->f0 * this->f0 * cos(this->gamma0) * (this->f0 * this->dt + sin(this->gamma0)) / (den * den);
+    Eigen::Vector3d omega_RN_R;
+    omega_RN_R << 0, 0, thetaDot;
+    Eigen::Vector3d omegaDot_RN_R;
+    omegaDot_RN_R << 0, 0, thetaDDot;
+
+    /*! populate attRefOut with reference frame information */
+    Eigen::Vector3d sigma_RN = dcmToMrp(RtN);
+
+    if (this->signOfOrbitNormalFrameVector == -1) {
+        Eigen::Vector3d halfRotationX;
+        halfRotationX << 1, 0, 0;
+        sigma_RN = addMrp(sigma_RN, halfRotationX);
+    }
+    Eigen::Vector3d omega_RN_N = RtN.transpose() * omega_RN_R;
+    Eigen::Vector3d omegaDot_RN_N = RtN.transpose() * omegaDot_RN_R;
+
+    return {sigma_RN, omega_RN_N, omegaDot_RN_N};
+}
+
+double FlybyPointAlgorithm::getTimeBetweenFilterData() const { return this->timeBetweenFilterData; }
+
+void FlybyPointAlgorithm::setTimeBetweenFilterData(double time) { this->timeBetweenFilterData = time; }
+
+double FlybyPointAlgorithm::getToleranceForCollinearity() const { return this->toleranceForCollinearity; }
+
+void FlybyPointAlgorithm::setToleranceForCollinearity(double tolerance) { this->toleranceForCollinearity = tolerance; }
+
+/*! Get the sign (+1 or -1) of the axis of rotation of the Z axis during the flyby
+ @param int sign (+1 or -1)
+ */
+int FlybyPointAlgorithm::getSignOfOrbitNormalFrameVector() const { return this->signOfOrbitNormalFrameVector; }
+
+/*! Set the sign (+1 or -1) of the axis of rotation of the Z axis during the flyby
+ @param int sign (+1 or -1)
+ */
+void FlybyPointAlgorithm::setSignOfOrbitNormalFrameVector(int sign) { this->signOfOrbitNormalFrameVector = sign; }
+
+/*! Get the maximum acceleration threshold to consider a solution invalid
+ @return double maximum accceleration
+ */
+double FlybyPointAlgorithm::getMaximumAccelerationThreshold() const { return this->maxAcceleration; }
+
+/*! Set the maximum acceleration threshold to consider a solution invalid
+ @param double maximum accceleration
+ */
+void FlybyPointAlgorithm::setMaximumAccelerationThreshold(double maxAccelerationThreshold) {
+    this->maxAcceleration = maxAccelerationThreshold;
+}
+
+/*! Get the maximum rate threshold to consider a solution invalid
+ @return maximum rate
+ */
+double FlybyPointAlgorithm::getMaximumRateThreshold() const { return this->maxRate; }
+
+/*! Set the maximum rate threshold to consider a solution invalid
+ @param maximum rate
+ */
+void FlybyPointAlgorithm::setMaximumRateThreshold(double maxRateThreshold) { this->maxRate = maxRateThreshold; }
+
+/*! Get the ground based positional knowledge standard deviation
+ @return sigma
+ */
+double FlybyPointAlgorithm::getPositionKnowledgeSigma() const { return this->positionKnowledgeSigma; }
+
+/*! Set the ground based positional knowledge sigma
+ @param sigma
+ */
+void FlybyPointAlgorithm::setPositionKnowledgeSigma(double positionKnowledgeStd) {
+    this->positionKnowledgeSigma = positionKnowledgeStd;
+}
---
+++ src/fswAlgorithms/attGuidance/flybyPoint/flybyPointAlgorithm.h
@@ -0,0 +1,71 @@
+/*
+ ISC License
+
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#ifndef _FLYBY_POINT_ALGORITHM_H
+#define _FLYBY_POINT_ALGORITHM_H
+
+#include "architecture/msgPayloadDef/AttRefMsgPayload.h"
+#include <Eigen/Dense>
+
+/*! @brief A class to perform flyby pointing */
+class FlybyPointAlgorithm {
+   public:
+    void reset();
+    AttRefMsgPayload updateState(uint64_t currentSimNanos,
+                                 const Eigen::Vector3d &r_BN_N,
+                                 const Eigen::Vector3d &v_BN_N);
+    bool checkValidity(uint64_t currentSimNanos, const Eigen::Vector3d &r_BN_N, const Eigen::Vector3d &v_BN_N) const;
+    void computeFlybyParameters(const Eigen::Vector3d &r_BN_N, const Eigen::Vector3d &v_BN_N);
+    void computeRN(const Eigen::Vector3d &r_BN_N, const Eigen::Vector3d &v_BN_N);
+    std::tuple<Eigen::Vector3d, Eigen::Vector3d, Eigen::Vector3d> computeGuidanceSolution() const;
+    double getTimeBetweenFilterData() const;
+    void setTimeBetweenFilterData(double timeBetweenFilterData);
+    double getToleranceForCollinearity() const;
+    void setToleranceForCollinearity(double toleranceForCollinearity);
+    int getSignOfOrbitNormalFrameVector() const;
+    void setSignOfOrbitNormalFrameVector(int signOfOrbitNormalFrameVector);
+    double getMaximumAccelerationThreshold() const;
+    void setMaximumAccelerationThreshold(double maxAccelerationThreshold);
+    double getMaximumRateThreshold() const;
+    void setMaximumRateThreshold(double maxRateThreshold);
+    double getPositionKnowledgeSigma() const;
+    void setPositionKnowledgeSigma(double positionKnowledgeStd);
+
+   private:
+    double dt = 0;                     //!< current time step between last two updates
+    double timeOfFirstRead = 0;        //!< time of first nav solution read
+    double timeBetweenFilterData = 0;  //!< time between two subsequent reads of the filter information
+    double toleranceForCollinearity =
+        0;  //!< tolerance for singular conditions when position and velocity are collinear
+    int signOfOrbitNormalFrameVector = 1;  //!< Sign of orbit normal vector to complete reference frame
+
+    double maxRate = 0;          //!< maximum rate spacecraft can control to, used for validity of solution
+    double maxAcceleration = 0;  //!< maximum acceleration spacecraft can control to, used for validity of solution
+
+    bool firstRead = true;               //!< variable to attest if this is the first read after a Reset
+    double f0 = 0;                       //!< ratio between relative velocity and position norms at time of read [Hz]
+    double gamma0 = 0;                   //!< flight path angle of the spacecraft at time of read [rad]
+    uint64_t lastFilterReadTime = 0;     //!< time of last filter read
+    Eigen::Matrix3d R0N;                 //!< inertial-to-reference DCM at time of read
+    Eigen::Vector3d firstNavPosition{};  //!< First position used to create profile
+    Eigen::Vector3d firstNavVelocity{};  //!< First velocity used to create profile
+    double positionKnowledgeSigma = 0;   //!< Last position used to create profile
+};
+
+#endif
