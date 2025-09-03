f20bbcdd1c949ee02e8c7ff74630da88889002f0
Bushra Aldhanhani <bashora.aldnhani@gmail.com>
2025-09-03 14:10:24 -0600

    Update unit test and add 2 additional test

--- src/fswAlgorithms/effectorInterfaces/rwMotorVoltage/_UnitTest/test_rwMotorVoltage.py
+++ src/fswAlgorithms/effectorInterfaces/rwMotorVoltage/_UnitTest/test_rwMotorVoltage.py
@@ -50,23 +50,24 @@ def add_time_column(time, data):
 # Provide a unique test method name, starting with 'test_'.
 # The following 'parametrize' function decorator provides the parameters and expected results for each
 #   of the multiple test runs for this test.
-
-@pytest.mark.parametrize("use_large_voltage, use_availability, use_torque_loop, test_name", [
-       (False, False, False, "One")
-     , (True, False, False, "Two")
-     , (False, True, False, "Three")
-     , (False, False, True, "Four")
+@pytest.mark.parametrize("use_large_voltage, use_availability, use_torque_loop, use_high_speed, test_name", [
+    (False, False, False, False, "One")
+    , (True,  False, False, False, "Two")
+    , (False, True,  False, False, "Three")
+    , (False, False, True,  False, "Four")
+    , (True, False, False,  True,  "Five")
+    , (False, False, False, True,  "Six")
 ])
 
 # update "module" in this function name to reflect the module name
-def test_module(show_plots, use_large_voltage, use_availability, use_torque_loop, test_name):
+def test_module(show_plots, use_large_voltage, use_availability, use_torque_loop, use_high_speed, test_name):
     """Module Unit Test"""
     # each test method requires a single assert method to be called
-    [testResults, testMessage] = run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_name)
+    [testResults, testMessage] = run(show_plots, use_large_voltage, use_availability, use_torque_loop, use_high_speed, test_name)
     assert testResults < 1, testMessage
 
 
-def run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_name):
+def run(show_plots, use_large_voltage, use_availability, use_torque_loop, use_high_speed, test_name):
     test_fail_count = 0                       # zero unit test result counter
     test_messages = []                       # create empty array to store test log messages
     unit_task_name = "unitTask"               # arbitrary name (don't change)
@@ -91,6 +92,7 @@ def run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_n
     # set module parameters
     module.VMin = 1.0     # Volts
     module.VMax = 11.0    # Volts
+    module.VMaxSat = 5.0    # Volts
 
     if use_torque_loop:
         module.K = 1.5
@@ -116,8 +118,8 @@ def run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_n
     for i in range(4):
         fswSetupRW.create(gs_matrix_b[i],        #           spin axis
                           0.1,              # kg*m^2    J2
-                          0.2)              # Nm        uMax
-
+                          0.2,              # Nm        uMax
+                          418.879)          # rad/s     TorqueSatSpeedLimit
     rw_config_in_msg = fswSetupRW.write_config_message()
     module.rwParamsInMsg.subscribeTo(rw_config_in_msg)
     num_rw = fswSetupRW.get_num_of_devices()
@@ -141,6 +143,18 @@ def run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_n
         rw_avail_in_msg = messaging.RWAvailabilityMsg().write(rw_availability_message)
         module.rwAvailInMsg.subscribeTo(rw_avail_in_msg)
 
+    if use_high_speed:
+        rw_speed_message = messaging.RWSpeedMsgPayload()
+        rw_speed_message.wheelSpeeds = [500.0, 500.0, -500., -500.]     # rad/sec Omega's
+        rw_speed_in_msg = messaging.RWSpeedMsg().write(rw_speed_message)
+        module.rwSpeedInMsg.subscribeTo(rw_speed_in_msg)
+        us_message_data.motorTorque = [0.05, -0.05, -0.05, 0.05]  # [Nm] RW motor torque cmds
+        rw_motor_torque_in_msg = messaging.ArrayMotorTorqueMsg().write(us_message_data)
+        module.torqueInMsg.subscribeTo(rw_motor_torque_in_msg)
+        if use_large_voltage:
+            us_message_data.motorTorque = [0.3, -0.3, -0.15, 0.15]  # [Nm] RW motor torque cmds
+            rw_motor_torque_in_msg = messaging.ArrayMotorTorqueMsg().write(us_message_data)
+            module.torqueInMsg.subscribeTo(rw_motor_torque_in_msg)
 
     # Setup logging on the test module output message so that we get all the writes to it
     data_log = module.voltageOutMsg.recorder()
@@ -181,9 +195,8 @@ def run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_n
 
 
     # set the filtered output truth states
-
     true_vector=[]
-    if not use_large_voltage and not use_availability and not use_torque_loop:
+    if not use_large_voltage and not use_availability and not use_torque_loop and not use_high_speed:
         true_vector = [
                    [3.5, 0., -8.5, -11.]
                  , [3.5, 0., -8.5, -11.]
@@ -193,8 +206,7 @@ def run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_n
                  , [3.5, 0., -8.5, -11.]
                  , [3.5, 0., -8.5, -11.]
                    ]
-
-    if use_large_voltage and not use_availability and not use_torque_loop :
+    if use_large_voltage and not use_availability and not use_torque_loop and not use_high_speed:
         true_vector = [
                    [11., 0., -8.5, -11.]
                  , [11., 0., -8.5, -11.]
@@ -204,8 +216,7 @@ def run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_n
                  , [11., 0., -8.5, -11.]
                  , [11., 0., -8.5, -11.]
                    ]
-
-    if not use_large_voltage and use_availability and not use_torque_loop:
+    if not use_large_voltage and use_availability and not use_torque_loop and not use_high_speed:
         true_vector = [
                    [3.5, 0., 0., -11.]
                  , [3.5, 0., 0., -11.]
@@ -215,8 +226,7 @@ def run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_n
                  , [3.5, 0., 0., -11.]
                  , [3.5, 0., 0., -11.]
                    ]
-
-    if not use_large_voltage and not use_availability and use_torque_loop:
+    if not use_large_voltage and not use_availability and use_torque_loop and not use_high_speed:
         true_vector = [
                    [3.5, 0., -8.5, -11.]
                  , [3.5, 0., -8.5, -11.]
@@ -226,6 +236,27 @@ def run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_n
                  , [3.5, 0., -8.5, -11.]
                  , [7.25, 0., -11., -11.]
                    ]
+    if  use_large_voltage and not use_availability and not use_torque_loop and use_high_speed:
+        true_vector = [
+                  [5., -5., -5., 5.]
+                , [5., -5., -5., 5.]
+                , [5., -5., -5., 5.]
+                , [5., -5., -5., 5.]
+                , [5., -5., -5., 5.]
+                , [5., -5., -5., 5.]
+                , [5., -5., -5., 5.]
+            ]
+    if  not use_large_voltage and not use_availability and not use_torque_loop and use_high_speed:
+        true_vector = [
+              [ 3.5, -3.5,  -3.5 ,  3.5]
+            , [ 3.5, -3.5,  -3.5 ,  3.5]
+            , [ 3.5, -3.5,  -3.5 ,  3.5]
+            , [ 3.5, -3.5,  -3.5 ,  3.5]
+            , [ 3.5, -3.5,  -3.5 ,  3.5]
+            , [ 3.5, -3.5,  -3.5 ,  3.5]
+            , [ 3.5, -3.5,  -3.5 ,  3.5]
+        ]
+
 
     # compare the module results to the truth values
     accuracy = 1e-10
@@ -236,19 +267,6 @@ def run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_n
 
 
 
-    # If the argument provided at commandline "--show_plots" evaluates as true,
-    # plot all figures
-    # plot a sample variable.
-    # plt.close("all")    # close all prior figures so we start with a clean slate
-    # plt.figure(1)
-    # plt.plot(variableState[:, 0]*macros.NANO2SEC, variableState[:, 1],
-    #          label='Case useLargeVoltage = ' + str(useLargeVoltage))
-    # plt.legend(loc='upper left')
-    # plt.xlabel('Time [s]')
-    # plt.ylabel('Variable Description [unit]')
-    # if show_plots:
-    #     plt.show()
-    #     plt.close('all')
 
     #   print out success message if no error were found
     snippent_name = "passFail" + test_name
@@ -295,8 +313,9 @@ def run(show_plots, use_large_voltage, use_availability, use_torque_loop, test_n
 if __name__ == "__main__":
     test_module(              # update "module" in function name
                   False
-                 ,False       # useLargeVoltage
-                 ,False       # useAvailability
-                 ,True        # useTorqueLoop
-                 ,"Four"      # testName
+                 ,False       # use large voltage
+                 ,False         # use availability
+                 ,False        # use torque Loop
+                 , True
+                 ,"Six"      # testName
                )
