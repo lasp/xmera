77da533451309f31a13522b76691085fa1a5f213
Julian Hammerl <julian.hammerl@hotmail.com>
2025-09-10 15:21:14 -0600

    Remove BSK Logger

--- src/fswAlgorithms/attGuidance/inertial3D/inertial3D.h
+++ src/fswAlgorithms/attGuidance/inertial3D/inertial3D.h
@@ -23,7 +23,6 @@
 #include "architecture/_GeneralModuleFiles/sys_model.h"
 #include "architecture/messaging/messaging.h"
 #include "architecture/msgPayloadDef/AttRefMsgPayload.h"
-#include "architecture/utilities/bskLogging.h"
 #include <stdint.h>
 
 /*!@brief Data structure for module to compute the Inertial-3D pointing navigation solution.
@@ -33,7 +32,6 @@ class Inertial3D : public SysModel {
     void updateState(uint64_t callTime) override;
     double sigma_R0N[3];                     //!<  MRP from inertial frame N to corrected reference frame R
     Message<AttRefMsgPayload> attRefOutMsg;  //!< reference attitude output message
-    BSKLogger bskLogger = {};                //!< BSK Logging
 };
 
 #endif
