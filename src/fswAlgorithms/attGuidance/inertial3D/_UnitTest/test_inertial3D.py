7760d035211c082a54d6baea8a81fcce2491aa74
Julian Hammerl <julian.hammerl@hotmail.com>
2025-09-11 10:01:55 -0600

    Update unit test

--- src/fswAlgorithms/attGuidance/inertial3D/_UnitTest/test_inertial3D.py
+++ src/fswAlgorithms/attGuidance/inertial3D/_UnitTest/test_inertial3D.py
@@ -1,7 +1,7 @@
 #
 #  ISC License
 #
-#  Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+#  Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
 #
 #  Permission to use, copy, modify, and/or distribute this software for any
 #  purpose with or without fee is hereby granted, provided that the above
@@ -15,23 +15,15 @@
 #  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 #  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
-#
-#   Unit Test Script
-#   Module Name:        inertial3D
-#   Author:             Mar Cols
-#   Creation Date:      January 6, 2015
-#
 
 import inspect
 import os
 
+import numpy as np
+import pytest
+
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(filename))
-
-
-
-
-
 
 # Import all of the modules that we are going to be called in this simulation
 from Basilisk.utilities import SimulationBaseClass
@@ -41,23 +33,10 @@ from Basilisk.utilities import macros
 from Basilisk.architecture import messaging
 
 
-# uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
-# @pytest.mark.skipif(conditionstring)
-# uncomment this line if this test has an expected failure, adjust message as needed
-# @pytest.mark.xfail(conditionstring)
-# provide a unique test method name, starting with test_
-def test_inertial3D(show_plots):
-    """Module Unit Test"""
-    # each test method requires a single assert method to be called
-    [testResults, testMessage] = subModuleTestFunction(show_plots)
-    assert testResults < 1, testMessage
-
-
-def subModuleTestFunction(show_plots):
-    testFailCount = 0                       # zero unit test result counter
-    testMessages = []                       # create empty array to store test log messages
-    unitTaskName = "unitTask"               # arbitrary name (don't change)
-    unitProcessName = "TestProcess"         # arbitrary name (don't change)
+@pytest.mark.parametrize("set_SigmaRN", [True, False])
+def test_inertial3D(show_plots, set_SigmaRN):
+    unitTaskName = "unitTask"
+    unitProcessName = "TestProcess"
 
     # Create a sim module as an empty container
     unitTestSim = SimulationBaseClass.SimBaseClass()
@@ -67,120 +46,44 @@ def subModuleTestFunction(show_plots):
     testProc = unitTestSim.CreateNewProcess(unitProcessName)
     testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
 
-
     # Construct algorithm and associated C++ container
     module = inertial3D.Inertial3D()
     module.modelTag = "inertial3D"
 
+    if set_SigmaRN:
+        vector = np.random.rand(3)
+        module.setSigmaR0N(vector)
+    else:
+        vector = [0.0, 0.0, 0.0]
+
     # Add test module to runtime call list
     unitTestSim.AddModelToTask(unitTaskName, module)
-
-    vector = [0.1, 0.2, 0.3]
-    module.setSigmaR0N(vector)
 
     # Setup logging on the test module output message so that we get all the writes to it
     dataLog = module.attRefOutMsg.recorder()
     unitTestSim.AddModelToTask(unitTaskName, dataLog)
 
-
-    # Need to call the self-init and cross-init methods
     unitTestSim.InitializeSimulation()
-
-    # Set the simulation time.
-    # NOTE: the total simulation time may be longer than this value. The
-    # simulation is stopped at the next logging event on or after the
-    # simulation end time.
     unitTestSim.ConfigureStopTime(macros.sec2nano(1.))        # seconds to stop simulation
-
-    # Begin the simulation time run set above
     unitTestSim.ExecuteSimulation()
 
-    # This pulls the actual data log from the simulation run.
-    # Note that range(3) will provide [0, 1, 2]  Those are the elements you get from the vector (all of them)
-    #
-    # check sigma_BR
-    #
-    moduleOutput = dataLog.sigma_RN
+    # retrieve the module output
+    sigma_RN = dataLog.sigma_RN
+    omega_RN_N = dataLog.omega_RN_N
+    domega_RN_N = dataLog.domega_RN_N
+
     # set the filtered output truth states
-    trueVector = [
-               [0.1, 0.2, 0.3],
-               [0.1, 0.2, 0.3],
-               [0.1, 0.2, 0.3]
-               ]
+    sigma_RN_truth = [vector] * 3
+    omega_RN_N_truth = [[0.0, 0.0, 0.0]] * 3
+    domega_RN_N_truth = [[0.0, 0.0, 0.0]] * 3
 
     # compare the module results to the truth values
     accuracy = 1e-12
-    for i in range(0,len(trueVector)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqual(moduleOutput[i],trueVector[i],3,accuracy):
-            testFailCount += 1
-            testMessages.append("FAILED: " + module.modelTag + " Module failed sigma_RN unit test at t=" +
-                                str(moduleOutput[i,0]*macros.NANO2SEC) +
-                                "sec\n")
-    #
-    # check omega_RN_N
-    #
-    moduleOutput = dataLog.omega_RN_N
-    # set the filtered output truth states
-    trueVector = [
-               [0.0, 0.0, 0.0],
-               [0.0, 0.0, 0.0],
-               [0.0, 0.0, 0.0]
-               ]
 
-    # compare the module results to the truth values
-    accuracy = 1e-12
-    unitTestSupport.writeTeXSnippet("toleranceValue", str(accuracy), path)
-
-    for i in range(0,len(trueVector)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqual(moduleOutput[i],trueVector[i],3,accuracy):
-            testFailCount += 1
-            testMessages.append("FAILED: " + module.modelTag + " Module failed omega_RN_N unit test at t=" +
-                                str(moduleOutput[i,0]*macros.NANO2SEC) +
-                                "sec\n")
-
-    #
-    # check domega_RN_B
-    #
-    moduleOutput = dataLog.domega_RN_N
-    # set the filtered output truth states
-    trueVector = [
-               [0.0, 0.0, 0.0],
-               [0.0, 0.0, 0.0],
-               [0.0, 0.0, 0.0]
-               ]
-
-    # compare the module results to the truth values
-    accuracy = 1e-12
-    for i in range(0,len(trueVector)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqual(moduleOutput[i],trueVector[i],3,accuracy):
-            testFailCount += 1
-            testMessages.append("FAILED: " + module.modelTag + " Module failed domega_RN_N unit test at t=" +
-                                str(moduleOutput[i,0]*macros.NANO2SEC) +
-                                "sec\n")
-
-    snippentName = "passFail"
-    if testFailCount == 0:
-        colorText = 'ForestGreen'
-        print("PASSED: " + module.modelTag)
-        passedText = r'\textcolor{' + colorText + '}{' + "PASSED" + '}'
-    else:
-        colorText = 'Red'
-        print("Failed: " + module.modelTag)
-        passedText = r'\textcolor{' + colorText + '}{' + "Failed" + '}'
-    unitTestSupport.writeTeXSnippet(snippentName, passedText, path)
+    np.testing.assert_allclose(sigma_RN, sigma_RN_truth, rtol=0, atol=accuracy, verbose=True)
+    np.testing.assert_allclose(omega_RN_N, omega_RN_N_truth, rtol=0, atol=accuracy, verbose=True)
+    np.testing.assert_allclose(domega_RN_N, domega_RN_N_truth, rtol=0, atol=accuracy, verbose=True)
 
 
-    # each test method requires a single assert method to be called
-    # this check below just makes sure no sub-test failures were found
-    return [testFailCount, ''.join(testMessages)]
-
-
-#
-# This statement below ensures that the unitTestScript can be run as a
-# stand-along python script
-#
 if __name__ == "__main__":
     test_inertial3D(False)
