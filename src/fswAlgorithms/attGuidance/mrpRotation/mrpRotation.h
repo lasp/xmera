c53789f495171d35ca5049e574c06cb54ac1b16d
Julian Hammerl <julian.hammerl@hotmail.com>
2025-09-17 11:40:03 -0600

    Move private class members from public to private

--- src/fswAlgorithms/attGuidance/mrpRotation/mrpRotation.h
+++ src/fswAlgorithms/attGuidance/mrpRotation/mrpRotation.h
@@ -38,22 +38,22 @@ class MrpRotation : public SysModel {
                                      double omega_R0N_N[3],
                                      double domega_R0N_N[3],
                                      AttRefMsgPayload *attRefOut);
-    /* Declare module public variables */
+
     double mrpSet[3];       //!< [-] current MRP attitude coordinate set with respect to the input reference
     double omega_RR0_R[3];  //!< [rad/s] angular velocity vector relative to input reference
-    /* Declare module private variables */
+
+    Message<AttRefMsgPayload> attRefOutMsg;     //!< The name of the output message containing the Reference
+    ReadFunctor<AttRefMsgPayload> attRefInMsg;  //!< The name of the guidance reference input message
+    ReadFunctor<AttStateMsgPayload>
+        desiredAttInMsg;  //!< The name of the incoming message containing the desired EA set
+
+   private:
     double cmdSet[3];         //!< [] msg commanded initial MRP sigma_RR0 set with respect to input reference
     double cmdRates[3];       //!< [rad/s] msg commanded constant angular velocity vector omega_RR0_R
     double priorCmdSet[3];    //!< [] prior commanded MRP set
     double priorCmdRates[3];  //!< [rad/s] prior commanded angular velocity vector
     uint64_t priorTime;       //!< [ns] last time the guidance module is called
     double dt;                //!< [s] integration time-step
-
-    /* Declare module IO interfaces */
-    Message<AttRefMsgPayload> attRefOutMsg;     //!< The name of the output message containing the Reference
-    ReadFunctor<AttRefMsgPayload> attRefInMsg;  //!< The name of the guidance reference input message
-    ReadFunctor<AttStateMsgPayload>
-        desiredAttInMsg;  //!< The name of the incoming message containing the desired EA set
 };
 
 #endif
