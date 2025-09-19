9b5b8aef0db87540f83bb8ae3ae0ea0e638b73c7
Julian Hammerl <julian.hammerl@hotmail.com>
2025-09-19 12:06:13 -0600

    Create copy for C module

---
+++ src/fswAlgorithms/attControl/mrpSteering_C/mrpSteering_C.h
@@ -0,0 +1,50 @@
+/*
+ ISC License
+
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#ifndef MRP_STEERING_CONTROL_C_H
+#define MRP_STEERING_CONTROL_C_H
+
+#include "architecture/_GeneralModuleFiles/sys_model.h"
+#include "architecture/messaging/messaging.h"
+#include "architecture/msgPayloadDef/AttGuidMsgPayload.h"
+#include "architecture/msgPayloadDef/RateCmdMsgPayload.h"
+#include <stdint.h>
+
+/*! @brief Data structure for the MRP feedback attitude control routine. */
+class MrpSteering_C : public SysModel {
+   public:
+    void reset(uint64_t callTime) override;
+    void updateState(uint64_t callTime) override;
+
+    /* declare module public variables */
+    double K1;         //!< [rad/sec] Proportional gain applied to MRP errors
+    double K3;         //!< [rad/sec] Cubic gain applied to MRP error in steering saturation function
+    double omega_max;  //!< [rad/sec] Maximum rate command of steering control
+
+    uint32_t
+        ignoreOuterLoopFeedforward;  //!< []      Boolean flag indicating if outer feedforward term should be included
+
+    /* declare module IO interfaces */
+    Message<RateCmdMsgPayload> rateCmdOutMsg;  //!< rate command output message
+    ReadFunctor<AttGuidMsgPayload> guidInMsg;  //!< attitude guidance input message
+
+    BSKLogger bskLogger = {};  //!< BSK Logging
+};
+
+#endif
---
+++ src/fswAlgorithms/attControl/mrpSteering_C/mrpSteering_C.i
@@ -0,0 +1,39 @@
+/*
+ ISC License
+
+ Copyright (c) 2025, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+%module mrpSteering_C
+%{
+   #include "mrpSteering_C.h"
+%}
+
+%pythoncode %{
+    from Basilisk.architecture.swig_common_model import *
+%}
+
+%include "sys_model.i"
+%include "swig_conly_data.i"
+
+%include "mrpSteering_C.h"
+
+%include "architecture/msgPayloadDef/AttGuidMsgPayload.h"
+%include "architecture/msgPayloadDef/RateCmdMsgPayload.h"
+
+%pythoncode %{
+import sys
+protectAllClasses(sys.modules[__name__])
+%}
