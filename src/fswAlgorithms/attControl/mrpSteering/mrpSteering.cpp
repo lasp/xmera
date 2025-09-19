b0156dbd068451a21e7149f7c28857e5ca838191
Julian Hammerl <julian.hammerl@hotmail.com>
2025-09-19 12:25:39 -0600

    Move steering law into updateState

--- src/fswAlgorithms/attControl/mrpSteering/mrpSteering.cpp
+++ src/fswAlgorithms/attControl/mrpSteering/mrpSteering.cpp
@@ -27,8 +27,6 @@
 #include <math.h>
 #include <stdexcept>
 
-static void MRPSteeringLaw(MrpSteering *configData, double sigma_BR[3], double omega_ast[3], double omega_ast_p[3]);
-
 /*! This method performs a complete reset of the module.  Local module variables that retain
  time varying states between function calls are reset to their default values.
  @return void
@@ -57,26 +55,12 @@ void MrpSteering::updateState(uint64_t callTime)
     /*! - Read the dynamic input messages */
     guidCmd = this->guidInMsg();
 
-    /*! - evalute MRP kinematic steering law */
-    MRPSteeringLaw(this, guidCmd.sigma_BR, outMsg.omega_BastR_B, outMsg.omegap_BastR_B);
+    double sigma_BR[3];
+    double omega_ast[3];
+    double omega_ast_p[3];
 
-    /*! - Store the output message and pass it to the message bus */
-    this->rateCmdOutMsg.write(&outMsg, moduleID, callTime);
+    v3Copy(guidCmd.sigma_BR, sigma_BR);
 
-    return;
-}
-
-/*! This method computes the MRP Steering law.  A commanded body rate is returned given the MRP
- attitude error measure of the body relative to a reference frame.  The function returns the commanded
- body rate, as well as the body frame derivative of this rate command.
- @return void
- @param this  The configuration data associated with this module
- @param sigma_BR    MRP attitude error of B relative to R
- @param omega_ast   Commanded body rates
- @param omega_ast_p Body frame derivative of the commanded body rates
- */
-void MRPSteeringLaw(MrpSteering *configData, double sigma_BR[3], double omega_ast[3], double omega_ast_p[3])
-{
     double  sigma_i;        /* ith component of sigma_B/R */
     double  B[3][3];        /* B-matrix of MRP differential kinematic equations */
     double  sigma_p[3];     /* MRP rates */
@@ -86,22 +70,29 @@ void MRPSteeringLaw(MrpSteering *configData, double sigma_BR[3], double omega_as
     /* Equation (18): Determine the desired steering rates  */
     for (i=0;i<3;i++) {
         sigma_i  = sigma_BR[i];
-        value        = atan(M_PI_2/configData->omega_max*(configData->K1*sigma_i
-                       + configData->K3*sigma_i*sigma_i*sigma_i))/M_PI_2*configData->omega_max;
+        value = atan(M_PI_2/this->omega_max*(this->K1*sigma_i
+                       + this->K3*sigma_i*sigma_i*sigma_i))/M_PI_2*this->omega_max;
         omega_ast[i] = -value;
     }
     v3SetZero(omega_ast_p);
-    if (!configData->ignoreOuterLoopFeedforward) {
+    if (!this->ignoreOuterLoopFeedforward) {
         /* Equation (21): Determine the body frame derivative of the steering rates */
         BmatMRP(sigma_BR, B);
         m33MultV3(B, omega_ast, sigma_p);
         v3Scale(0.25, sigma_p, sigma_p);
         for (i=0;i<3;i++) {
             sigma_i  = sigma_BR[i];
-            value = (3*configData->K3*sigma_i*sigma_i + configData->K1)/
-                                (pow(M_PI_2/configData->omega_max*(configData->K1*sigma_i + configData->K3*sigma_i*sigma_i*sigma_i),2) + 1);
+            value = (3*this->K3*sigma_i*sigma_i + this->K1)/
+                                (pow(M_PI_2/this->omega_max*(this->K1*sigma_i + this->K3*sigma_i*sigma_i*sigma_i),2) + 1);
             omega_ast_p[i] = - value*sigma_p[i];
         }
     }
+
+    v3Copy(omega_ast, outMsg.omega_BastR_B);
+    v3Copy(omega_ast_p, outMsg.omegap_BastR_B);
+
+    /*! - Store the output message and pass it to the message bus */
+    this->rateCmdOutMsg.write(&outMsg, moduleID, callTime);
+
     return;
 }
