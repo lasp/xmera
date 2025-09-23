5cb7effb2000fe0ff337193dacb42f275d58b0c6
Julian Hammerl <julian.hammerl@hotmail.com>
2025-09-23 13:25:36 -0600

    Move private class members from public to private

--- src/fswAlgorithms/attControl/mrpFeedback/mrpFeedback.h
+++ src/fswAlgorithms/attControl/mrpFeedback/mrpFeedback.h
@@ -42,14 +42,7 @@ class MrpFeedback : public SysModel {
     double Ki;                    //!< [N*m]     Integration feedback error on rate error
     double integralLimit;         //!< [N*m]     Integration limit to avoid wind-up issue
     int controlLawType;           //!<           Flag to choose between the two control laws available
-    uint64_t priorTime;           //!< [ns]      Last time the attitude control is called
-    double z[3];                  //!< [rad]     integral state of delta_omega
-    double int_sigma[3];          //!< [s]       integral of the MPR attitude error
     double knownTorquePntB_B[3];  //!< [N*m]     known external torque in body frame vector components
-
-    double ISCPntB_B[9];  //!< [kg m^2]  Spacecraft Inertia
-    RWArrayConfigMsgPayload
-        rwConfigParams;  //!< [-] struct to store message containing RW config parameters in body B frame
 
     /* declare module IO interfaces */
     ReadFunctor<RWSpeedMsgPayload> rwSpeedsInMsg;        //!< RW speed input message (Optional)
@@ -60,6 +53,14 @@ class MrpFeedback : public SysModel {
         intFeedbackTorqueOutMsg;                          //!< commanded integral feedback control torque output message
     ReadFunctor<AttGuidMsgPayload> guidInMsg;             //!< attitude guidance input message
     ReadFunctor<VehicleConfigMsgPayload> vehConfigInMsg;  //!< vehicle configuration input message
+
+   private:
+    uint64_t priorTime{};           //!< [ns]      Last time the attitude control is called
+    double z[3];                  //!< [rad]     integral state of delta_omega
+    double int_sigma[3];          //!< [s]       integral of the MPR attitude error
+    double ISCPntB_B[9];  //!< [kg m^2]  Spacecraft Inertia
+    RWArrayConfigMsgPayload
+        rwConfigParams{};  //!< [-] struct to store message containing RW config parameters in body B frame
 };
 
 #endif
