ce96f04967dde6dae628168a31e93a3471ce712b
Safa Al Hosani <safa.alhosani@lasp.colorado.edu>
2025-10-02 18:54:16 -0600

    update unit test

--- src/fswAlgorithms/imageProcessing/centerOfBrightness/_UnitTest/test_centerOfBrightness.py
+++ src/fswAlgorithms/imageProcessing/centerOfBrightness/_UnitTest/test_centerOfBrightness.py
@@ -69,13 +69,34 @@ def compute_window_size(window_point_top_left, window_point_bottom_right):
 
     return [width, height]
 
+def compute_brightness_and_pixel_refs(image, input_image,
+                                      window_point_top_left, window_point_bottom_right,
+                                      valid_image):
 
-def centerOfBrightnessTest(show_plots, image, blur, save_test, valid_image, save_image, window_point_top_left,
-                           window_point_bottom_right):
-    imagePath = path + '/' + image
-    imagePath_module = path + '/temp_' + image
-    input_image = Image.open(imagePath)
-    input_image.load()
+    cob_ref = [input_image.width/2, input_image.height/2]
+    pixelNum_ref = None
+
+    if image == "half_half.png":
+        # left half black, right half white, and a 1px wide grey stripe in the center (116/255)
+        white_width = 138
+        grey_width = 1
+        height = 183
+        if np.array_equal(window_point_top_left, [50, 0]) and np.array_equal(window_point_bottom_right, [275, 91]):
+            height = 91
+
+        cob_ref = [(3/4 * 1 * white_width + 1/2 * 116/255 * grey_width)/(white_width + grey_width) * input_image.width,
+                   int(height/2)*valid_image]
+        pixelNum_ref = ((white_width+grey_width)*height)*valid_image
+
+        return cob_ref, pixelNum_ref, white_width, grey_width, height
+
+    return cob_ref, pixelNum_ref, None, None, None
+
+
+
+def run_sequence(image, blur, save_test, valid_image,
+                         window_point_top_left, window_point_bottom_right,
+                         image_path, image_path_module):
 
     # setup simulation environment
     unit_test_sim = SimulationBaseClass.SimBaseClass()
@@ -87,7 +108,7 @@ def centerOfBrightnessTest(show_plots, image, blur, save_test, valid_image, save
     window_center = compute_window_center(window_point_top_left, window_point_bottom_right)
     window_width, window_height = compute_window_size(window_point_top_left, window_point_bottom_right)
     module_config = centerOfBrightness.CenterOfBrightness()
-    module_config.modelTag = "centerOfBrightness"
+    module_config.modelTag = "centerOfBrightness_seq"
     if window_center.all() != 0 and window_width != 0 and window_height != 0:
         module_config.setWindowCenter(window_center)
         module_config.setWindowSize(window_width, window_height)
@@ -111,35 +132,33 @@ def centerOfBrightnessTest(show_plots, image, blur, save_test, valid_image, save
 
     unit_test_sim.AddModelToTask("unit_task", module_config)
 
-    cob_ref = [input_image.width/2, input_image.height/2]
+    input_message_data = messaging.CameraImageMsgPayload()
+    input_message_data.timeTag = int(1E9)
+    input_message_data.cameraID = 1
+    input_message_data.valid = valid_image
+    img_in_msg = messaging.CameraImageMsg().write(input_message_data)
+    module_config.imageInMsg.subscribeTo(img_in_msg)
+
+    data_log = module_config.opnavCOBOutMsg.recorder()
+    unit_test_sim.AddModelToTask("unit_task", data_log)
+
+    # run simulation for 5 time steps (excluding initial time step at 0 ns), scale brightness each time step
+    # necessary to test rolling brightness average
+
+    unit_test_sim.InitializeSimulation()
+    scaler = np.array([0.5, 0.6, 0.8, 0.3, 0.9])
+    brightness_ref = np.zeros([len(scaler)])
+    brightnessAverage_ref = np.zeros([len(scaler)])
+
+
+    white_width = grey_width = height = None
     if image == "half_half.png":
-        # left half black, right half white, and a 1px wide grey stripe in the center with brightness 116/255
         white_width = 138
         grey_width = 1
         height = 183
         if np.array_equal(window_point_top_left, [50, 0]) and np.array_equal(window_point_bottom_right, [275, 91]):
             height = 91
 
-        cob_ref = [(3/4 * 1 * white_width + 1/2 * 116/255 * grey_width)/(white_width + grey_width) * input_image.width,
-                   int(height/2)*valid_image]
-        pixelNum_ref = ((white_width+grey_width)*height)*valid_image
-
-    inputMessageData = messaging.CameraImageMsgPayload()
-    inputMessageData.timeTag = int(1E9)
-    inputMessageData.cameraID = 1
-    inputMessageData.valid = valid_image
-    imgInMsg = messaging.CameraImageMsg().write(inputMessageData)
-    module_config.imageInMsg.subscribeTo(imgInMsg)
-    dataLog = module_config.opnavCOBOutMsg.recorder()
-    unit_test_sim.AddModelToTask("unit_task", dataLog)
-
-    unit_test_sim.InitializeSimulation()
-
-    # run simulation for 5 time steps (excluding initial time step at 0 ns), scale brightness each time step
-    # necessary to test rolling brightness average
-    scaler = np.array([0.5, 0.6, 0.8, 0.3, 0.9])
-    brightness_ref = np.zeros([len(scaler)])
-    brightnessAverage_ref = np.zeros([len(scaler)])
     for i in range(0, len(scaler)):
         im = cv2.imread(image_path)
         th, im_th = cv2.threshold(im, int(scaler[i] * 255), 255, cv2.THRESH_TRUNC)
@@ -148,16 +167,38 @@ def centerOfBrightnessTest(show_plots, image, blur, save_test, valid_image, save
         unit_test_sim.ConfigureStopTime(i * process_rate)
         unit_test_sim.ExecuteSimulation()
 
-        # true rolling brightness average
         if image == "half_half.png":
             brightness_raw = int(scaler[i] * 255)*white_width*height + 116*grey_width*height
             brightness_ref[i] = brightness_raw / 255
             lower_idx = max(0, i-(int(module_config.getNumberOfPointsBrightnessAverage())-1))
             brightnessAverage_ref[i] = np.mean(brightness_ref[lower_idx:i+1])
 
-    center = dataLog.centerOfBrightness[0, :]
-    pixelNum = dataLog.pixelsFound[0]
-    brightnessAverage = dataLog.rollingAverageBrightness
+    center = data_log.centerOfBrightness[0, :]
+    pixelNum = data_log.pixelsFound[0]
+    brightnessAverage = data_log.rollingAverageBrightness
+
+    return center, pixelNum, brightnessAverage, brightnessAverage_ref
+
+
+def centerOfBrightnessTest(show_plots, image, blur, save_test, valid_image, save_image, window_point_top_left,
+                           window_point_bottom_right):
+    image_path = path + '/' + image
+    image_path_module = path + '/temp_' + image
+    input_image = Image.open(image_path)
+    input_image.load()
+
+    cob_ref, pixelNum_ref, _, _, _ = compute_brightness_and_pixel_refs(
+        image, input_image, window_point_top_left, window_point_bottom_right, valid_image
+    )
+
+    center, pixelNum, brightnessAverage, brightnessAverage_ref = run_sequence(
+        image, blur, save_test, valid_image,
+        window_point_top_left, window_point_bottom_right,
+        image_path, image_path_module
+    )
+
+    window_center = compute_window_center(window_point_top_left, window_point_bottom_right)
+    window_width, window_height = compute_window_size(window_point_top_left, window_point_bottom_right)
 
     output_image = Image.new("RGB", input_image.size)
     output_image.paste(input_image)
@@ -173,23 +214,19 @@ def centerOfBrightnessTest(show_plots, image, blur, save_test, valid_image, save
 
     input_image.close()
 
-    # Remove saved images for the test of that functionality
     files = glob.glob(path + "/result_*")
     for f in files:
         os.remove(f)
-    # Save output image with center of brightness
     if save_image:
         output_image.save("result_" + image)
 
     if show_plots:
         output_image.show()
 
-    # Remove temporarily created images
     files = glob.glob(image_path_module)
     for f in files:
         os.remove(f)
 
-    # make sure module output data is correct
     tolerance = 0.6  # just above half a pixel
     np.testing.assert_allclose(center,
                                cob_ref,
@@ -212,6 +249,183 @@ def centerOfBrightnessTest(show_plots, image, blur, save_test, valid_image, save
                                    atol=0,
                                    err_msg='Variable: brightnessAverage',
                                    verbose=True)
+
+def test_windowing():
+
+    image = "window_test.png"
+    image_path = os.path.join(path, image)
+    pixel_threshold = 50
+    blur_size = 1
+    atol_px = 0.6
+    valid_image = True
+
+    image_w = Image.open(image_path)
+    w, h = image_w.size
+    expected_full = np.array([w / 2, h / 2])
+
+    image_gray = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    top_image = image_gray[0:h // 2, 0:w]
+    top_bright_pixels = (top_image >= pixel_threshold).astype(np.float64)
+    y, x = np.nonzero(top_bright_pixels)
+    expected_top = np.array([x.mean(), y.mean()])
+    
+    # full image 
+    unit_test_sim_full = SimulationBaseClass.SimBaseClass()
+    process_rate = macros.sec2nano(0.5)
+    test_process_full = unit_test_sim_full.CreateNewProcess("window_process_A")
+    test_process_full.addTask(unit_test_sim_full.CreateNewTask("window_task_A", process_rate))
+
+    module_config_full = centerOfBrightness.CenterOfBrightness()
+    module_config_full.modelTag = "centerOfBrightness_window_full"
+    module_config_full.setRelativeBrightnessIncreaseThreshold(0.0)
+    module_config_full.setNumberOfPointsBrightnessAverage(1)
+    module_config_full.setFileName(image_path)
+    module_config_full.setBlurSize(blur_size)
+    module_config_full.setPixelThreshold(pixel_threshold)
+    module_config_full.setSaveDir(path + '/result_save.png')
+    module_config_full.setSaveImages(False)
+
+    input_message_data_full = messaging.CameraImageMsgPayload()
+    input_message_data_full.timeTag = int(1E9); input_message_data_full.cameraID = 1;
+    input_message_data_full.valid = valid_image
+    imgIninput_message_data_full = messaging.CameraImageMsg().write(input_message_data_full)
+    module_config_full.imageInMsg.subscribeTo(imgIninput_message_data_full)
+
+    data_log_full = module_config_full.opnavCOBOutMsg.recorder()
+    unit_test_sim_full.AddModelToTask("window_task_A", module_config_full)
+    unit_test_sim_full.AddModelToTask("window_task_A", data_log_full)
+
+    unit_test_sim_full.InitializeSimulation()
+    unit_test_sim_full.ConfigureStopTime(process_rate)
+    unit_test_sim_full.ExecuteSimulation()
+
+    center_full = data_log_full.centerOfBrightness[0, :]
+
+    # Top-half window
+    unit_test_sim_window = SimulationBaseClass.SimBaseClass()
+    test_process_window = unit_test_sim_window.CreateNewProcess("window_process_B")
+    test_process_window.addTask(unit_test_sim_window.CreateNewTask("window_task_B", process_rate))
+
+    module_config_window = centerOfBrightness.CenterOfBrightness()
+    module_config_window.modelTag = "centerOfBrightness_window_test_top"
+    module_config_window.setRelativeBrightnessIncreaseThreshold(0.0)
+    module_config_window.setNumberOfPointsBrightnessAverage(1)
+    module_config_window.setFileName(image_path)
+    module_config_window.setBlurSize(blur_size)
+    module_config_window.setPixelThreshold(pixel_threshold)
+    module_config_window.setSaveDir(path + '/result_save.png')
+    module_config_window.setSaveImages(False)
+
+    # Define and set top-half
+    x0 = 0
+    y0 = 0
+    x1 = w
+    y1 = h // 2
+    windowed_center = np.array([int((x0 + x1) / 2), int((y0 + y1) / 2)])
+    windowed_size = (int(x1 - x0), int(y1 - y0))
+    module_config_window.setWindowCenter(windowed_center)
+    module_config_window.setWindowSize(windowed_size[0], windowed_size[1])
+
+    input_message_data_window = messaging.CameraImageMsgPayload()
+    input_message_data_window.timeTag = int(1E9); input_message_data_window.cameraID = 1;
+    input_message_data_window.valid = valid_image
+    imgIninput_message_data_window = messaging.CameraImageMsg().write(input_message_data_window)
+    module_config_window.imageInMsg.subscribeTo(imgIninput_message_data_window)
+
+    data_log_window = module_config_window.opnavCOBOutMsg.recorder()
+    unit_test_sim_window.AddModelToTask("window_task_B", module_config_window)
+    unit_test_sim_window.AddModelToTask("window_task_B", data_log_window)
+
+    unit_test_sim_window.InitializeSimulation()
+    unit_test_sim_window.ConfigureStopTime(process_rate)
+    unit_test_sim_window.ExecuteSimulation()
+
+    center_top = data_log_window.centerOfBrightness[0, :]
+
+    np.testing.assert_allclose(
+        center_full, expected_full, rtol=0.0, atol=atol_px,
+        err_msg="COB mismatch for full-frame (no window)", verbose=True
+    )
+    np.testing.assert_allclose(
+        center_top, expected_top, rtol=0.0, atol=atol_px,
+        err_msg="COB mismatch for top-half window", verbose=True
+    )
+
+def test_threshold():
+    image = "threshold_test.png"
+    image_path = os.path.join(path, image)
+    blur_size = 1
+    atol_px = 0.6
+    valid_image = True
+
+    unit_test_sim_threshold = SimulationBaseClass.SimBaseClass()
+    test_process_threshold = unit_test_sim_threshold.CreateNewProcess("threshold_process")
+    process_rate = macros.sec2nano(0.5)
+    test_process_threshold.addTask(unit_test_sim_threshold.CreateNewTask("threshold_task", process_rate))
+
+    module_config_threshold = centerOfBrightness.CenterOfBrightness()
+    module_config_threshold.modelTag = "centerOfBrightness_threshold_test"
+    module_config_threshold.setRelativeBrightnessIncreaseThreshold(0.0)
+    module_config_threshold.setNumberOfPointsBrightnessAverage(1)
+    module_config_threshold.setFileName(image_path)
+    module_config_threshold.setBlurSize(blur_size)
+    module_config_threshold.setPixelThreshold(50)
+    module_config_threshold.setSaveDir(path + '/result_save.png')
+    module_config_threshold.setSaveImages(False)
+
+    input_message_data_threshold = messaging.CameraImageMsgPayload()
+    input_message_data_threshold.timeTag = int(1E9); input_message_data_threshold.cameraID = 1;
+    input_message_data_threshold.valid = valid_image
+    imgIninput_message_data_threshold = messaging.CameraImageMsg().write(input_message_data_threshold)
+    module_config_threshold.imageInMsg.subscribeTo(imgIninput_message_data_threshold)
+
+    data_log_threshold = module_config_threshold.opnavCOBOutMsg.recorder()
+    unit_test_sim_threshold.AddModelToTask("threshold_task", module_config_threshold)
+    unit_test_sim_threshold.AddModelToTask("threshold_task", data_log_threshold)
+
+    unit_test_sim_threshold.InitializeSimulation()
+    unit_test_sim_threshold.ConfigureStopTime(process_rate)
+    unit_test_sim_threshold.ExecuteSimulation()
+
+    cob_50 = data_log_threshold.centerOfBrightness[0, :]
+
+# find cob for 100
+
+    unit_test_sim_threshold_100 = SimulationBaseClass.SimBaseClass()
+    test_process_threshold_100 = unit_test_sim_threshold_100.CreateNewProcess("threshold_100_process")
+    process_rate = macros.sec2nano(0.5)
+    test_process_threshold_100.addTask(unit_test_sim_threshold_100.CreateNewTask("threshold_100_task", process_rate))
+
+    module_config_threshold_100 = centerOfBrightness.CenterOfBrightness()
+    module_config_threshold_100.modelTag = "centerOfBrightness_threshold_100_test"
+    module_config_threshold_100.setRelativeBrightnessIncreaseThreshold(0.0)
+    module_config_threshold_100.setNumberOfPointsBrightnessAverage(1)
+    module_config_threshold_100.setFileName(image_path)
+    module_config_threshold_100.setBlurSize(blur_size)
+    module_config_threshold_100.setPixelThreshold(100)
+    module_config_threshold_100.setSaveDir(path + '/result_save.png')
+    module_config_threshold_100.setSaveImages(False)
+
+    input_message_data_threshold_100 = messaging.CameraImageMsgPayload()
+    input_message_data_threshold_100.timeTag = int(1E9); input_message_data_threshold_100.cameraID = 1;
+    input_message_data_threshold_100.valid = valid_image
+    imgIninput_message_data_threshold_100 = messaging.CameraImageMsg().write(input_message_data_threshold_100)
+    module_config_threshold_100.imageInMsg.subscribeTo(imgIninput_message_data_threshold_100)
+
+    data_log_threshold_100 = module_config_threshold_100.opnavCOBOutMsg.recorder()
+    unit_test_sim_threshold_100.AddModelToTask("threshold_100_task", module_config_threshold_100)
+    unit_test_sim_threshold_100.AddModelToTask("threshold_100_task", data_log_threshold_100)
+
+    unit_test_sim_threshold_100.InitializeSimulation()
+    unit_test_sim_threshold_100.ConfigureStopTime(process_rate)
+    unit_test_sim_threshold_100.ExecuteSimulation()
+
+    cob_100 = data_log_threshold_100.centerOfBrightness[0, :]
+
+    x_cob_50 = cob_50[0]
+    x_cob_100 = cob_100[0]
+
+    assert x_cob_100 > x_cob_50, "COB did not move right as threshold increased:"
 
 
 if __name__ == "__main__":
