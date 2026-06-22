e50c8985b80c841986d738058c1dc1c2f0edb812
Jonathan Castello <twisolar@gmail.com>
2026-06-22 03:13:05 -0700

    Document and refactor SysModelTask

--- src/architecture/system_model/sys_model_task.cpp
+++ src/architecture/system_model/sys_model_task.cpp
@@ -4,113 +4,56 @@
 
 #include "sys_model_task.h"
 
-/*! A construction option that allows the user to set some task parameters.
- Note that the only required argument is InputPeriod.
- @param InputPeriod The amount of nanoseconds between calls to this Task.
- @param FirstStartTime The amount of time in nanoseconds to hold a task dormant before starting.
-        After this time the task is executed at integer amounts of InputPeriod again
- */
-SysModelTask::SysModelTask(uint64_t InputPeriod, uint64_t FirstStartTime)
-    : NextStartTime(FirstStartTime), TaskPeriod(InputPeriod), FirstTaskTime(FirstStartTime) {}
-
-//! Project a time onto this task's lattice of scheduled updates
-/*!
- *  Let `p` be `TaskPeriod` and `k` be `FirstTaskTime.
- *  Every update of this task is some multiple of `p` plus an offset `k`.
- *  In other words, every update `u` is `p*n + k` for some non-negative `n`.
- *
- *  This method takes an arbitrary time `t` and finds the latest `u` that is
- *  no later than `t`.
- */
 inline uint64_t SysModelTask::projectToCurrentSchedule(uint64_t time) {
-    // Judge everything relative to the origin, `FirstTaskTime`.
-    time -= this->FirstTaskTime;
+    // Judge everything relative to the origin, `firstUpdateNanos`.
+    time -= this->firstUpdateNanos;
 
-    // Truncate `time` to the nearest mutiple of `TaskPeriod`.
-    time = (time / this->TaskPeriod) * this->TaskPeriod;
+    // Truncate `time` to the nearest mutiple of `updatePeriodNanos`.
+    time = (time / this->updatePeriodNanos) * this->updatePeriodNanos;
 
-    // Re-add the offset against `FirstTaskTime`.
-    time += this->FirstTaskTime;
+    // Re-add the offset against `firstUpdateNanos`.
+    time += this->firstUpdateNanos;
 
     return time;
 }
 
-/*! This method resets all of the models that have been added to the Task at the CurrentSimTime.
- * See sys_model_task.h for related method reset()
- @return void
- @param CurrentSimTime The time to start at after reset
-*/
-void SysModelTask::resetModels(uint64_t CurrentSimTime) {
-    for (auto const& modelPair : this->TaskModels) {
-        modelPair.ModelPtr->reset(CurrentSimTime);
-    }
+void SysModelTask::resetModels(uint64_t initialSimNanos) {
+    for (auto const &modelPair : this->TaskModels) { modelPair.ModelPtr->reset(initialSimNanos); }
 
     // Make sure we stick to our intended schedule.
-    this->NextStartTime = this->projectToCurrentSchedule(CurrentSimTime);
-    if (this->NextStartTime < CurrentSimTime) { this->NextStartTime += this->TaskPeriod; }
+    this->nextUpdateNanos = this->projectToCurrentSchedule(initialSimNanos);
+    if (this->nextUpdateNanos < initialSimNanos) { this->nextUpdateNanos += this->updatePeriodNanos; }
 }
 
-/*! This method executes all of the models on the Task during runtime.
- Then, it sets its NextStartTime appropriately.
- @return void
- @param currentSimNanos The current simulation time in [ns]
- */
-void SysModelTask::executeModels(uint64_t currentSimNanos) {
-    this->NextStartTime += this->TaskPeriod;
+void SysModelTask::executeModels(uint64_t nextSimNanos) {
+    this->nextUpdateNanos += this->updatePeriodNanos;
 
     if (!this->taskActive) { return; }
 
-    for (auto &modelPair : this->TaskModels) { modelPair.ModelPtr->updateState(currentSimNanos); }
+    for (auto &modelPair : this->TaskModels) { modelPair.ModelPtr->updateState(nextSimNanos); }
 }
 
-/*! This method adds a new model into the Task list.  Note that the Priority
- parameter is option as it defaults to -1 (lowest, latest)
- @return void
- @param NewModel The new model that we are adding to the Task
- @param Priority The selected priority of the model being added (highest goes first)
- */
-void SysModelTask::addModel(SysModel* NewModel, int32_t Priority) {
-    ModelPriorityPair LocalPair;
-
-    //! - Set the local pair with the requested priority and mode
-    LocalPair.CurrentModelPriority = Priority;
-    LocalPair.ModelPtr = NewModel;
-
-    //! - Loop through the ModelPair vector and if Priority is higher than next, insert
-    for (auto ModelPair = this->TaskModels.begin(); ModelPair != this->TaskModels.end(); ModelPair++) {
-        if (Priority > ModelPair->CurrentModelPriority) {
-            this->TaskModels.insert(ModelPair, LocalPair);
-            return;
-        }
+void SysModelTask::addModel(SysModel* module, int32_t priority) {
+    // Find the index separating lower priorities from higher priorities.
+    auto it = this->TaskModels.begin();
+    for (; it != this->TaskModels.end(); ++it) {
+        if (priority > it->CurrentModelPriority) { break; }
     }
-    //! - If we make it to the end of the loop, this is lowest priority, put it at end
-    this->TaskModels.push_back(LocalPair);
+
+    // Insert the module at this index. (It's okay if it's the end() iterator.)
+    this->TaskModels.insert(it, {.CurrentModelPriority = priority, .ModelPtr = module});
 }
 
-/*! This method changes the period of a given task over to the requested period.
-   It attempts to keep the same offset relative to the original offset that
-   was specified at task creation.
- @return void
- @param newPeriod The period that the task should run at going forward
- */
-void SysModelTask::setPeriod(uint64_t newPeriod) {
-    //! - If the requested time is above the min time, set the next time based on the previous time plus the new period
-    if (this->NextStartTime > this->FirstTaskTime) {
-        uint64_t lastStartTime = this->NextStartTime - this->TaskPeriod;
+void SysModelTask::setPeriod(uint64_t updatePeriodNanos) {
+    if (this->nextUpdateNanos > this->firstUpdateNanos) {
+        uint64_t lastUpdateTime = this->nextUpdateNanos - this->updatePeriodNanos;
 
-        this->TaskPeriod = newPeriod;
-        this->NextStartTime = this->projectToCurrentSchedule(this->NextStartTime);
-        if (newStartTime <= lastStartTime) { this->NextStartTime += this->TaskPeriod; }
-    }
-    //! - Otherwise, we just should keep the original requested first call time for the task
-    else {
-        this->TaskPeriod = newPeriod;
-        this->NextStartTime = this->FirstTaskTime;
+        this->updatePeriodNanos = updatePeriodNanos;
+        this->nextUpdateNanos = this->projectToCurrentSchedule(this->nextUpdateNanos);
+        if (this->nextUpdateNanos <= lastUpdateTime) { this->nextUpdateNanos += updatePeriodNanos; }
+    } else {
+        // If we haven't yet performed an update, our next update time is *still*
+        // our first update time.
+        this->updatePeriodNanos = updatePeriodNanos;
     }
 }
-
-uint64_t SysModelTask::getNextStartTime() const { return this->NextStartTime; }
-
-uint64_t SysModelTask::getTaskPeriod() const { return this->TaskPeriod; }
-
-uint64_t SysModelTask::getFirstTaskTime() const { return this->FirstTaskTime; }
--- src/architecture/system_model/sys_model_task.h
+++ src/architecture/system_model/sys_model_task.h
@@ -10,55 +10,194 @@
 #include <stdint.h>
 #include <vector>
 
-//! Structure used to pair a model and its requested priority
-struct ModelPriorityPair {
-    int32_t CurrentModelPriority;  //!< The current model priority. Higher goes first
-    SysModel* ModelPtr;            //!< The model associated with this priority
+//! A module paired with its priority among modules within its containing task
+struct ModelPriorityPair final {
+    //! The priority of this module among others within its containing task
+    int32_t CurrentModelPriority;
+
+    //! A non-owning pointer to a module
+    SysModel* ModelPtr;
 };
 
-//! Class used to group a set of models into one "Task" of execution
-class SysModelTask {
+//! An ordered sequence of modules to be updated on a periodic schedule
+/*!
+ *  An instance of `SysModelTask` (a "task") is a sequence of modules that should
+ *  always be updated as a unit during simulation. Each module is inserted into
+ *  the sequence by priority, with higher-priority modules being updated before
+ *  lower-priority modules.
+ *
+ *  Every task also includes an update period, which dictates how frequently its
+ *  modules will be updated, and an initial update time, which indicates the time
+ *  at which the task should begin updating. The task will not update at any point
+ *  prior to the first update time; however, once this time is reached, it will
+ *  continue to update according to the update period.
+ *
+ *  A task may also be enabled or disabled. When disabled, the modules in this
+ *  task will be inhibited from updating, though future updates will continue to
+ *  occur on the same schedule. In other words, if a task is disabled and later
+ *  re-enabled, the updates occurring afterwards will occur on schedule as if
+ *  the task was never disabled, although updates while the task was disabled
+ *  will not have occurred at all.
+ *
+ *  @todo \parblock
+ *    This ought to be an opaque `SysModel`. It's doing two jobs: it represents
+ *    a sequential composition of other `SysModel`s, and it manages its own update
+ *    schedule. These jobs ought to be separated out, and arguably the latter
+ *    should be the job of the containing `SysProcess`. Heck, `SysProcess` *already*
+ *    keeps a copy of the `NextTaskStart` and `TaskUpdatePeriod` for each task.
+ *
+ *    On top of that, `resetModules()` and `reset()` are weirdly separated when
+ *    neither should be invoked without the other also. If those can be combined,
+ *    then `resetModules()` and `executeModules()` are exactly implementing
+ *    `SysModel::reset()` and `SysModel::updateState()`.
+ *  \endparblock
+ */
+class SysModelTask final {
 public:
-    SysModelTask() = default;
-    ~SysModelTask() = default;
+    //! Obtain an empty task with a given update period and start time
+    /*!
+     *  @param[in] updatePeriodNanos
+     *    The interval of nanoseconds that should elapse between updates
+     *  @param[in] firstUpdateNanos
+     *    The time at which the task should be first updated
+     */
+    explicit SysModelTask(uint64_t updatePeriodNanos = 100, uint64_t firstUpdateNanos = 0)
+        : nextUpdateNanos(firstUpdateNanos), updatePeriodNanos(updatePeriodNanos), firstUpdateNanos(firstUpdateNanos) {}
 
-    explicit SysModelTask(uint64_t InputPeriod, uint64_t FirstStartTime = 0);
+    //! Insert a module into the task's sequence of modules with a given priority
+    /*!
+     *  @param[in] module
+     *    A non-owning pointer to the module to be added to the task
+     *  @param[in] priority
+     *    The priority of the given module (higher is earlier).
+     */
+    void addModel(SysModel* module, int32_t priority = -1);
 
-    void addModel(SysModel* NewModel, int32_t Priority = -1);
-    void executeModels(uint64_t CurrentSimTime);
-    void resetModels(uint64_t CurrentSimTime);
-    void setPeriod(uint64_t newPeriod);
+    //! Update all modules in the task, in priority order
+    /*!
+     *  The argument to this method should always be equal to `getNextUpdateTime()`.
+     *  On return, `getNextUpdateTime()` will indicate the next time at which this
+     *  method should be called.
+     *
+     *  @param[in] nextSimNanos
+     *    The simulation instant associated with the desired simulation state
+     */
+    void executeModels(uint64_t nextSimNanos);
 
+    //! Reset all modules in the task, in priority order
+    /*!
+     *  @param[in] initialSimNanos
+     *    The simulation instant associated with the initial simulation state
+     */
+    void resetModels(uint64_t initialSimNanos);
+
+    //! Change the update period of the task
+    /*!
+     *  If the task has already been updated since its last reset, the next update
+     *  time is recalculated against the new period. On return, the task will
+     *  follow an update schedule as if its period were always `updatePeriodNanos`.
+     *  The value of `getNextUpdateTime()` will be the greatest lower bound of
+     *  its previous value on the new schedule; or if that wouldn't be later than
+     *  the actual last time the module was updated, it will be the least upper
+     *  bound instead.
+     *
+     *  This method may make the next time update earlier than it was before changing
+     *  the period. Hence, it is safest to invoke this method at the same simulation
+     *  time as (but causally after) an update occurs.
+     *
+     *  @param updatePeriodNanos
+     *    The new interval of nanoseconds that should elapse between updates
+     */
+    void setPeriod(uint64_t updatePeriodNanos);
+
+    //! Roll back the next update time to the *first* update time
+    /*!
+     *  This method must be used in conjunction with `resetModels()`, and must
+     *  not be used while a simulation is still in progress.
+     *
+     *  @todo
+     *    This method should be folded into `resetModels()`; it's silly that they're
+     *    separate. Invoking one method without the other would decohere the
+     *    simulation time understood by the task from the simulation time understood
+     *    by its modules.
+     */
     void reset() {
-        this->NextStartTime = this->FirstTaskTime;
+        this->nextUpdateNanos = this->firstUpdateNanos;
     }
 
+    //! Get the next scheduled update time for this task
+    /*!
+     *  This will always be `getTaskPeriod()` plus some multiple of `getFirstTaskTime()`.
+     */
+    uint64_t getNextStartTime() const {
+        return this->nextUpdateNanos;
+    }
+
+    //! Permit the task's modules to be updated at subsequent update times
     void enable() {
         this->taskActive = true;
     }
 
+    //! Inhibit the task's modules from being updated at subsequent update times
     void disable() {
         this->taskActive = false;
     }
 
-    uint64_t getNextStartTime() const;
-    uint64_t getTaskPeriod() const;
-    uint64_t getFirstTaskTime() const;
+    //! Get the task's current update period in nanoseconds
+    uint64_t getTaskPeriod() const {
+        return this->updatePeriodNanos;
+    }
+
+    //! Get the task's initial update time in nanoseconds
+    uint64_t getFirstTaskTime() const {
+        return this->firstUpdateNanos;
+    }
 
 private:
+    //! Project a time onto this task's lattice of scheduled updates
+    /*!
+     *  Let `p` be `TaskPeriod` and `k` be `FirstTaskTime`.
+     *  Every update of this task is some multiple of `p` plus an offset `k`.
+     *  In other words, every update `u` is `p*n + k` for some non-negative `n`.
+     *
+     *  This method takes an arbitrary time `t` and finds the latest `u` that is
+     *  no later than `t`.
+     */
     uint64_t projectToCurrentSchedule(uint64_t time);
 
 public:
-    std::vector<ModelPriorityPair> TaskModels{};  //!< -- Array that has pointers to all task sysModels
-    std::string TaskName{};                       //!< -- Identifier for Task
-    bool taskActive = true;                       //!< -- Flag indicating whether the Task has been disabled
-    BSKLogger bskLogger{};                        //!< -- BSK Logging
+    //! The sequence of modules in this task, ordered by priority
+    /*!
+     *  @important
+     *    This vector *must* remain in sorted priority order. It should never
+     *    be modified by clients of `SysModelTask`.
+     *
+     *  @todo
+     *    This should be `private`. Why is this not `private`??
+     */
+    std::vector<ModelPriorityPair> TaskModels{};
+
+    //! A configurable, human-readable name for this task
+    std::string TaskName = "";
+
+    //! Whether the task's modules will be updated at subsequent update times
+    /*!
+     *  @todo
+     *    Either this field should be made `private` and a getter method added,
+     *    or this field should be made `public` and the `enable()` and `disable()`
+     *    methods removed. (I vote for the latter.)
+     */
+    bool taskActive = true;
 
 private:
-    uint64_t NextStartTime = 0;  //!< [ns] Next time to start task
-    uint64_t TaskPeriod = 100;   //!< [ns] Cycle rate for Task
-    uint64_t FirstTaskTime = 0;  //!< [ns] Time to start Task for first time.
-                                 //!<      After this time the normal periodic updates resume.
+    //! The next time (in nanoseconds) at which the task's modules will be updated
+    uint64_t nextUpdateNanos;
+
+    //! The interval of nanoseconds that will elapse between updates
+    uint64_t updatePeriodNanos;
+
+    //! The first time (in nanoseconds) at which the task's modules will be updated
+    uint64_t const firstUpdateNanos;
 };
 
 #endif
