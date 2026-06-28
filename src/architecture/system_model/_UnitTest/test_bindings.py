b5be0a953d06a7a4ebec0234319f776c6a104885
Jonathan Castello <twisolar@gmail.com>
2026-06-28 00:11:41 -0700

    Add a smoke test for the Python/C++ bindings against SimModel

---
+++ src/architecture/system_model/_UnitTest/test_bindings.py
@@ -0,0 +1,23 @@
+from xmera.architecture import sim_model
+
+def test_SimModel_bindings():
+    """
+    Check that the expected FFI bindings for SimModel are exposed
+    """
+    sim = sim_model.SimModel()
+
+    process = sim_model.SysProcess("process name")
+    assert sim.addNewProcess(process) is None
+
+    assert sim.resetSimulation() is None
+    assert sim.singleStepProcesses() is None
+    assert sim.stepUntilStop(0) is None
+
+    assert type(sim.getNextTaskTime()) is int
+    assert type(sim.getCurrentNanos()) is int
+    assert type(sim.getNextProcPriority()) is int
+
+    assert type(len(sim.processList)) is int
+    assert type(sim.processList[0]) is sim_model.SysProcess
+    for p in sim.processList:
+        assert type(p) is sim_model.SysProcess
