66ed694fda05bcfcf69634103350f21084c610ec
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-08-11 14:01:39 -0600

    Assign each variable in a single expression

--- src/simulation/dynamics/_GeneralModuleFiles/stateEffector.cpp
+++ src/simulation/dynamics/_GeneralModuleFiles/stateEffector.cpp
@@ -5,7 +5,9 @@
 
 StateEffector::StateEffector() {
     // - set force and torques equal to zero
-    this->forceOnBody_B = this->torqueOnBodyPntB_B = this->torqueOnBodyPntC_B.setZero();
+    this->forceOnBody_B.setZero();
+    this->torqueOnBodyPntB_B.setZero();
+    this->torqueOnBodyPntC_B.setZero();
 
     this->nameOfSpacecraftAttachedTo = "";
     this->r_BP_P.setZero();
